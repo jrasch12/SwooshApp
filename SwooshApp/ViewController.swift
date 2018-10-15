7c87170294a3a0912ac9e87ca3faf628cc1ccb22
Josh Rasch <raschnuts@macbook-pro.local>
2018-10-15 18:54:33 -0400

    Initial Commit

---
+++ SwooshApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwooshApp
+//
+//  Created by Josh Rasch on 10/15/18.
+//  Copyright © 2018 Josh Rasch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
